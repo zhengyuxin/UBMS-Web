3e32a9b72496a51b136456610232ae1e64b4cfb8
yzhen22 <yzhen22@YZHEN22-MOBL1.ccr.corp.intel.com>
2014-08-21 12:18:35 +0800

    Pilot 0.18 add celery demo add environment build manual

---
+++ Docs/celery_demo/celeryconfig.py
@@ -0,0 +1,12 @@
+BROKER_URL = 'amqp://guest@localhost//'
+# CELERY_RESULT_BACKEND = 'db+sqlite:///results.sqlite'
+CELERY_RESULT_BACKEND = 'amqp'
+
+CELERY_IMPORTS = ("tasks", )
+
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_SERIALIZER = 'json'
+# CELERY_ACCEPT_CONTENT=['json']
+CELERY_TIMEZONE = 'Europe/Oslo'
+CELERY_ENABLE_UTC = True
+CELERYD_CONCURRENCY = 3
